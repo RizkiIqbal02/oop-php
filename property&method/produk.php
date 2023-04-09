50fb03f721d4e8a58f7d163e65eb0b878eeb9d96
Sullivan <roxashiqbal12@gmail.com>
2023-04-09 14:15:08 +0700

    menambahkan folder property&method untuk melanjutkan study kasus2

---
+++ property&method/produk.php
@@ -0,0 +1,11 @@
+<?php  
+
+//nama class harus sama dengan nama file (sama seperti rules java)
+class Produk  {
+    public $judul, $penulis, $penerbit, $harga;
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
+
+?>
