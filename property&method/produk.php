b8c67a13ce1d1b28477fbc345cac1891bca59ee7
Sullivan <roxashiqbal12@gmail.com>
2023-04-09 15:03:12 +0700

    Menambahkan method getter pada class produk dan membuat beberapa object serta mengisi object tersebut

--- property&method/produk.php
+++ property&method/produk.php
@@ -3,14 +3,25 @@
 //nama class harus sama dengan nama file (sama seperti rules java)
 class Produk  {
     public $judul, $penulis, $penerbit, $harga;
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
 }
 
 $produk1 = new Produk();
 $produk1->judul = "Naruto";
 $produk1->penulis = "Masashi Kishimoto";
-$produk1->penerbit = "Shounen JUmp";
-$produk1->harga = "30000";
+$produk1->penerbit = "Shounen Jump";
+$produk1->harga = 30000;
 
-var_dump($produk1)
+$produk2 = new Produk();
+$produk2->judul = "Genshin Impact";
+$produk2->penulis = "Orang cina";
+$produk2->penerbit = "Mihoyo";
+$produk2->harga = 40000;
 
+echo 'Komik: '.$produk1->getLabel();
+echo "<br>";
+echo 'Game: '.$produk2->getLabel();
 ?>
